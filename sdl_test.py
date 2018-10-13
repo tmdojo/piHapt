ee2cd0981eb38354bdcea10b906b27f364e01693
Shunya Sato <shunya.s@jp.tdk.com>
2018-10-13 16:05:56 +0900

    add sdl test

---
+++ sdl_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+https://learn.adafruit.com/raspberry-pi-pygame-ui-basics/overview
+"""
+import os
+
+import pygame
+
+os.putenv('SDL_FBDEV', '/dev/fb1')
+pygame.init()
+lcd = pygame.display.set_mode((320, 240))
+lcd.fill((255,0,0))
+pygame.display.update()
+pygame.mouse.set_visible(False)
+while True:
+    pass
